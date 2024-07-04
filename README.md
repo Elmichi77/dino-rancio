0fe652eaefe2f868eee9164ac15fc7dd04f381f5
Elmichi77 <michigato.enzo@gmail.com>
2024-07-03 21:34:14 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Abra esta pagina en [https://elmichi77.github.io/dino-rancio/](https://elmichi77.github.io/dino-rancio/)
+
+## Usar como extensión
+
+Este repositorio puede ser añadido como una **extensión** en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **New Project**
+* haga clic en **Extensiones** en el menú del engranaje
+* buscar **https://github.com/elmichi77/dino-rancio** e importar
+
+## Edita este proyecto
+
+Para editar este repositorio en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **Import** luego haga clic en **Import URL**
+* pegue **https://github.com/elmichi77/dino-rancio** y haga clic en importar
+
+#### Metadatos (utilizados para búsqueda, renderizado)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,36 @@
+input.onButtonPressed(Button.A, function () {
+    Cabesaurio.change(LedSpriteProperty.Y, -1)
+    Patasaurio.change(LedSpriteProperty.Y, -1)
+    basic.pause(1000)
+    Cabesaurio.change(LedSpriteProperty.Y, 1)
+    Patasaurio.change(LedSpriteProperty.Y, 1)
+})
+function Piedrita () {
+    Piedra = game.createSprite(4, 4)
+    for (let index = 0; index < 4; index++) {
+        basic.pause(1000)
+        Piedra.change(LedSpriteProperty.X, -1)
+        if (Piedra.isTouching(Patasaurio)) {
+            basic.showIcon(IconNames.Sad)
+        }
+    }
+}
+input.onGesture(Gesture.Shake, function () {
+    game.resume()
+})
+input.onButtonPressed(Button.B, function () {
+    game.pause()
+})
+let Piedra: game.LedSprite = null
+let Cabesaurio: game.LedSprite = null
+let Patasaurio: game.LedSprite = null
+Patasaurio = game.createSprite(0, 4)
+Cabesaurio = game.createSprite(0, 3)
+Piedrita()
+basic.forever(function () {
+    if (Piedra.isTouchingEdge()) {
+        basic.pause(1000)
+        Piedra.delete()
+        Piedrita()
+    }
+})
